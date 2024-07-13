bc45200de3a47689a97be91345f474d6737ac383
Hidaya Wetsakhulia <hidayawetsa@gmail.com>
2024-07-13 11:17:59 +0300

    syle front-end

--- frontend/src/Pages/Kids.jsx
+++ frontend/src/Pages/Kids.jsx
@@ -23,21 +23,37 @@ const Kids = ({ categorytwo }) => {
   }, [categorytwo]);
 
   return (
-    <div>
-      <h1>{categorytwo} Products</h1>
-      <div>
-        {products.map(product => (
-          <div key={product.id}>
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-            <p>${product.price}</p>
-            <p><img src={product.image_url} alt={product.name} /></p>
-            {/* Example Link usage */}
-            {/* <Link to={/product/${product.id}}>View Details</Link> */}
+    <div className="bg-white">
+    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <h2 className="text-2xl font-bold tracking-tight text-gray-900">{categorytwo}Kid's collection</h2>
+
+      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        {products.map((product) => (
+          <div key={product.id} className="group relative">
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <img
+                alt={product.imageAlt}
+                src={product.image_url}
+                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+              />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href={product.href}>
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {product.name}
+                  </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+              </div>
+              <p className="text-sm font-medium text-gray-900">ksh{product.price}</p>
+            </div>
           </div>
         ))}
       </div>
     </div>
+  </div>
   );
 }
 
